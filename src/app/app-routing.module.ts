c893984fa5c2e2c07320ecde270628b2944a001c
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-22 19:11:33 -0300

    Feat: criado rota para tecnico-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children:[
-      {path: 'home', component:HomeComponent}
+      {path: 'home', component:HomeComponent},
+      {path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 ];
