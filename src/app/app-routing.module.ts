e691138f88c1f2bc4bba92dab3140b41a5df8038
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-24 07:54:09 -0300

    Feat: criada rota login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule, Component } from '@angular/core';
@@ -5,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  {path: 'login', component:LoginComponent},
   {
     path: '', component: NavComponent, children:[
       {path: 'home', component:HomeComponent},
