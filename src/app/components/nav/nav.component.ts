680522aa8a1a3e9cfbcbe62a9d3b10f1e3f7d017
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-25 14:32:43 -0300

    Feat: feito logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,10 +10,19 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(['tecnicos'])
+  }
+
+  logout(){
+    this.router.navigate(['login']);
+    this.authService.logout();
+    this.toast.info('Logout realizado com sucesso!', 'logout',{timeOut: 7000});
   }
 
 }
