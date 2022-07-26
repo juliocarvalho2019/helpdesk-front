e308e1735115d2f5f84c04fa76f3f80ce345a4b0
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-26 17:44:40 -0300

    Feat: Criado formuláuio de criação de um técnico, adicionado mascara cpf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,8 +36,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
-
-
+import { NgxMaskModule } from 'ngx-mask';
 
 
 
@@ -80,8 +79,11 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
       closeButton: true,
       progressBar: true
     }),
+    NgxMaskModule.forRoot()
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
