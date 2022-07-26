67ba8447073355500d22d9f09639762360417287
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-26 09:19:50 -0300

    Feat: criado rota para componente tecnico-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,7 +12,9 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, canActivate:[AuthGuard], children:[
       {path: 'home', component:HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+
+      {path: 'tecnicos', component: TecnicoListComponent},
+      {path: 'tecnicos/create', component: TecnicoCreateComponent}
     ]
   }
 ];
