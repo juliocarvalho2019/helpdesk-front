1c0657dc03a13cdd0fd78f85a5e32a28969d43d6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-28 09:35:45 -0300

    Feat:crud cliente finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
@@ -19,6 +23,11 @@ const routes: Routes = [
       {path: 'tecnicos/create', component: TecnicoCreateComponent},
       {path: 'tecnicos/update/:id', component: TecnicoUpdateComponent},
       {path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent},
+
+      {path: 'clientes', component: ClienteListComponent},
+      {path: 'clientes/create', component: ClienteCreateComponent},
+      {path: 'clientes/update/:id', component: ClienteUpdateComponent},
+      {path: 'clientes/delete/:id', component: ClienteDeleteComponent},
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,13 @@ import { NgxMaskModule } from 'ngx-mask';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+
+
+
 
 
 @NgModule({
@@ -53,6 +60,10 @@ import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecn
     TecnicoCreateComponent,
     TecnicoUpdateComponent,
     TecnicoDeleteComponent,
+    ClienteCreateComponent,
+    ClienteDeleteComponent,
+    ClienteListComponent,
+    ClienteUpdateComponent,
   ],
   imports: [
     BrowserModule,
