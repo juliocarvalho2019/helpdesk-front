3b8318ae7a6935c29ef81fc351ce354275a520a6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-28 16:28:33 -0300

    Feat: criado componente chamado create.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 
 
@@ -66,6 +67,7 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
     ClienteListComponent,
     ClienteUpdateComponent,
     ChamadoListComponent,
+    ChamadoCreateComponent,
   ],
   imports: [
     BrowserModule,
